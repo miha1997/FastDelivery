545265e703c9832c8bf0a7eab19d60a27899fbd6
miha1997 <mihailo.obradovic1997@gmail.com>
2019-05-25 22:01:44 +0200

    delivery-half

--- js/functions.js
+++ js/functions.js
@@ -196,6 +196,11 @@ function defaultSort(region, langugage){
 }
 
 function homeSort(langugage){
+	navs[0].classList.add('active');
+	if(navs[1].classList.contains('active')) navs[1].classList.replace('active', 'disabled');
+	if(navs[2].classList.contains('active')) navs[2].classList.replace('active', 'disabled');
+	if(navs[3].classList.contains('active')) navs[3].classList.replace('active', 'disabled');
+
 	$.getJSON( "base.json", function( json ) {
 		var restaurants = [];
 
@@ -248,4 +253,105 @@ function mySort(val, region, langugage){
 	if(val == 3)
 		sortByAddress(region, langugage);
 
+}
+
+function bigSort(langugage){
+	var links = document.getElementById('food-type');
+	var navs = links.getElementsByClassName('nav-link');
+
+	var italian = false, traditional = false, american = false;
+
+	if(navs[0].classList.contains('active')) italian = true;
+	if(navs[1].classList.contains('active')) traditional = true;		
+	if(navs[2].classList.contains('active')) american = true;
+
+
+	$.getJSON( "base.json", function( json ) {
+		var restaurants = [];
+
+		for(var i = 0; i < json.restaurants.length; i++)
+			if((json.restaurants[i].italian && italian) || (json.restaurants[i].traditional && traditional) || (json.restaurants[i].american && american))
+				restaurants.push(json.restaurants[i]);
+
+		links = document.getElementById('sort-type');
+		navs = links.getElementsByClassName('nav-link');
+		var sortType = 0;
+		if(navs[0].classList.contains('active')) sortType = 0;
+		if(navs[1].classList.contains('active')) sortType = 1;
+		if(navs[2].classList.contains('active')) sortType = 2;
+		if(navs[3].classList.contains('active')) sortType = 3;
+
+		if(sortType == 1){
+			for(var i = 0; i < restaurants.length - 1; i++)
+				for(var j = i + 1; j < restaurants.length; j++)
+					if(restaurants[i].name > restaurants[j].name){
+						var temp = restaurants[i];
+						restaurants[i] = restaurants[j];
+						restaurants[j] = temp;
+					}
+		}
+
+		if(sortType == 3){
+			for(var i = 0; i < restaurants.length - 1; i++)
+				for(var j = i + 1; j < restaurants.length; j++)
+					if(restaurants[i].address > restaurants[j].address){
+						var temp = restaurants[i];
+						restaurants[i] = restaurants[j];
+						restaurants[j] = temp;
+					}
+		}
+
+		if(sortType == 2){
+			for(var i = 0; i < restaurants.length - 1; i++)
+				for(var j = i + 1; j < restaurants.length; j++)
+					if(restaurants[i].rateSum/restaurants[i].rateNum < restaurants[j].rateSum/restaurants[j].rateNum){
+						var temp = restaurants[i];
+						restaurants[i] = restaurants[j];
+						restaurants[j] = temp;
+					}
+		}
+
+		if(navs[0].classList.contains('active'))navs[0].classList.replace('active', 'disabled');
+		if(navs[1].classList.contains('active'))navs[1].classList.replace('active', 'disabled');
+		if(navs[2].classList.contains('active'))navs[2].classList.replace('active', 'disabled');
+		if(navs[3].classList.contains('active'))navs[3].classList.replace('active', 'disabled');
+
+		var cnt = 0;
+
+		for(var i = 0; i < restaurants.length; i++){
+			var index = i + 1;
+			document.getElementById('name' + index).innerHTML= restaurants[i].name;
+			if(langugage == 'en')
+				document.getElementById('name' + index).href = restaurants[i].page_en;
+			else
+				document.getElementById('name' + index).href =restaurants[i].page_sr;
+			
+			if(langugage == 'en'){
+				document.getElementById('rate' + index).innerHTML= 'Rate: ' + restaurants[i].rateSum/restaurants[i].rateNum;
+				document.getElementById('address' + index).innerHTML= 'Address: ' + restaurants[i].address;
+			}
+			else{
+				document.getElementById('rate' + index).innerHTML= 'Ocena: ' + restaurants[i].rateSum/restaurants[i].rateNum;
+				document.getElementById('address' + index).innerHTML= 'Adresa: ' + restaurants[i].address;
+			}
+
+			document.getElementById('link' + index).style.backgroundImage = "url(" + restaurants[i].picture + ")";
+			if(langugage == 'en')
+				document.getElementById('link' + index).href = restaurants[i].page_en;
+			else
+				document.getElementById('link' + index).href =restaurants[i].page_sr;
+		}
+
+		var cnt = restaurants.length - 1;
+		cnt = cnt / 3;
+
+		for(var i = cnt + 1; i < 5; i++){
+			var toHide = document.getElementById("block" + i);  
+			toHide.style.setProperty('display', 'none', 'important');
+
+		}
+
+
+	});
+		
 }
